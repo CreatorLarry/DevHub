77bc0ec38d3669bd92e00c58a2e810a22025f60b
CreatorLarry <lawrencengugi90@gmail>
2025-12-14 13:40:36 +0300

    completed detail page, learning and implementing server action

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,13 @@ import ExploreBtn from "@/components/ExploreBtn";
 import EventCard from "@/components/EventCard";
 
 import events from "@/lib/constants"
+import {IEvent} from "@/database";
 
-const Page = () => {
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+const Page = async () => {
+    const response = await fetch(`${BASE_URL}/api/events`);
+    const {events} = await response.json();
+
     return (
         <section>
             <h1 className="text-center">
@@ -17,7 +22,7 @@ const Page = () => {
                 <h3>Featured Events</h3>
 
                 <ul className="events">
-                    {events && events.length > 0 && events.map((event) => (
+                    {events && events.length > 0 && events.map((event : IEvent) => (
                         <li key={event.title} className="list-none">
                             <EventCard {...event} />
                         </li>
