438114204a9fedf8484f713deb19ace2ac76171a
CreatorLarry <lawrencengugi90@gmail>
2025-12-17 02:39:57 +0300

    final deployment3

--- components/EventDetails.tsx
+++ components/EventDetails.tsx
@@ -101,7 +101,7 @@ const EventDetails = async ({params}: { params: Promise <string>}) => {
                             <p className="text-sm">Be the first to book your spot!</p>
                         )}
 
-                        <BookEvent eventId={event._id} slug={event.slug}/>
+                        <BookEvent eventId={event._id.toString()} slug={event.slug}/>
                     </div>
                 </aside>
             </div>
